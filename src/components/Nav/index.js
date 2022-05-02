f41c701b4c3e20ded4cfaa89a1f4f2d1b0649a1e
clarencepenz <clarencedespenz@gmail.com>
2022-05-02 03:36:59 +0100

    ++ Nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -31,7 +31,7 @@ export default function Nav({ logout }) {
 
   return (
     <Flex bg="beige" w="full">
-      <Link href="/near-announcement-ui" textDecoration="none" mr="auto">
+      <Link href="/near-announcement-ui" style={{ textDecoration: "none" }} mr="auto">
         <Image src={LOGO} h="50px" w="50px" ml="20px" />
       </Link>
       <Box>
@@ -45,16 +45,25 @@ export default function Nav({ logout }) {
             Ⓝ {balance}
           </MenuButton>
           <MenuList>
-            <MenuItem minH="48px">
+            <MenuItem minH="48px" _hover={{ bg: "teal.200" }}>
               <Avatar
-                 m={2} size="xs" name={`${account.accountId[0]}`} 
+                m={2}
+                size="xs"
+                name={`${account.accountId[0]}`}
                 mr="12px"
               />
-              <span style={{ color: '#000'}}>{account.accountId}</span>
+              <Link
+                href={`https://explorer.testnet.near.org/accounts/${account.accountId}`}
+                target="_blank"
+                color="#000"
+                style={{ textDecoration: "none" }}
+              >
+                {account.accountId}
+              </Link>
             </MenuItem>
-            <MenuItem minH="40px">
+            <MenuItem minH="40px" _hover={{ bg: "teal.200" }}>
               <span
-                style={{ color: '#000', textAlign: "center", width: "83%"}}
+                style={{ color: "#000", textAlign: "center", width: "83%" }}
                 onClick={() => logout()}
               >
                 Disconnect
